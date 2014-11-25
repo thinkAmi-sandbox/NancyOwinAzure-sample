7568987edefdbb82bb3b6ab786542451de9e9c8e
thinkAmi <dev.thinkami+git@gmail.com>
2014-11-25 19:03:48 +0900

    add auto-run FluentMigrator by .deployment file

--- deploy.cmd
+++ deploy.cmd
@@ -2,4 +2,4 @@ rem deploy to azure
 %MSBUILD_PATH% AspNetHosting\AspNetHosting.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TARGET%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Debug;SolutionDir="%DEPLOYMENT_SOURCE%"
 
 rem run FluentMigrator
-"\packages\FluentMigrator.Tools.1.3.0.0\tools\AnyCPU\40\Migrate.exe" /conn "%SQLAZURECONNSTR_NancyOwinAzure%" /provider sqlserver2012 /assembly "\AspNetHosting\bin\AspNetHosting.dll" /verbose true
+"%DEPLOYMENT_SOURCE%\packages\FluentMigrator.Tools.1.3.0.0\tools\AnyCPU\40\Migrate.exe" /conn "%SQLAZURECONNSTR_NancyOwinAzure%" /provider sqlserver2012 /assembly "%DEPLOYMENT_SOURCE%\AspNetHosting\bin\AspNetHosting.dll" /verbose true
